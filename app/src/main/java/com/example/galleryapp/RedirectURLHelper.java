0142bf06cfd4b1a63a0d594d2b6c027d42c03fb0
Abhi <abhishek7340128579@gmail.com>
2021-05-18 22:35:12 +0530

    Update

--- app/src/main/java/com/example/galleryapp/RedirectURLHelper.java
+++ app/src/main/java/com/example/galleryapp/RedirectURLHelper.java
@@ -1,10 +1,12 @@
 package com.example.galleryapp;
 
 import android.os.AsyncTask;
+import android.util.Log;
+
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class RedirectURLHelper extends AsyncTask<String, Void, Void> {
+public class RedirectURLHelper extends AsyncTask<String, Void, String> {
 
     private OnCompleteListener listener;
 
@@ -18,21 +20,33 @@ public class RedirectURLHelper extends AsyncTask<String, Void, Void> {
 
 
     @Override
-    protected Void doInBackground(String... strings) {
+    protected String doInBackground(String... strings) {
+            HttpURLConnection  httpURLConnection=null;
         try {
             URL imageUrl = new URL(strings[0]);
-            HttpURLConnection  httpURLConnection = (HttpURLConnection) imageUrl.openConnection();
+            httpURLConnection = (HttpURLConnection) imageUrl.openConnection();
             httpURLConnection.getResponseCode();
 
             httpURLConnection.disconnect();
-            listener.fetchRedirectUrl(httpURLConnection.getURL().toString());
+//            listener.fetchRedirectUrl(httpURLConnection.getURL().toString());
         } catch (Exception e) {
-            listener.OnFail();
+            Log.d("Abhi", "doInBackground: " );
+            return null;
+
         }
 
-        return null;
+        return httpURLConnection.getURL().toString();
     }
 
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        if(s==null){
+            listener.OnFail();
+            return;
+        }
+        listener.fetchRedirectUrl(s);
+    }
 
     interface OnCompleteListener {
         void fetchRedirectUrl(String s);
